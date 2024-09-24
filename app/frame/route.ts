10befbd251de2bc4b60ba77a9bc37110a711499c
Nikbaruch <130352420+Nikbaruch@users.noreply.github.com>
2024-09-24 15:37:24 +0300

    Update route.ts

--- app/frame/route.ts
+++ app/frame/route.ts
@@ -69,3 +69,22 @@ export async function POST(req: NextRequest, res: NextResponse) {
     return new NextResponse(frameMetadata);
   }
 }
+
+{
+  "untrustedData": {
+    "fid": 2,
+    "url": "https://fcpolls.com/polls/1",
+    "messageHash": "0xd2b1ddc6c88e865a33cb1a565e0058d757042974",
+    "timestamp": 1706243218,
+    "network": 1,
+    "buttonIndex": 2,
+    "inputText": "hello world", // "" if requested and no input, undefined if input not requested
+    "castId": {
+      "fid": 226,
+      "hash": "0xa48dd46161d8e57725f5e26e34ec19c13ff7f3b9"
+    }
+  },
+  "trustedData": {
+    "messageBytes": "d2b1ddc6c88e865a33cb1a565e0058d757042974..."
+  }
+}
