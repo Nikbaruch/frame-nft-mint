0916454f1b2ad5d79a657598f794bf1b5f5f1e5f
Nikbaruch <130352420+Nikbaruch@users.noreply.github.com>
2024-09-26 22:54:17 +0300

    Update route.ts

--- app/frame/route.ts
+++ app/frame/route.ts
@@ -1,8 +1,8 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server"
 import { getConnectedAddressForUser } from "@/utils/fc";
 import { mintNft, balanceOf } from "@/utils/mint";
-import { PinataFDK } from "pinata-fdk";
 
+import { PinataFDK } from "pinata-fdk";
 const fdk = new PinataFDK({
   pinata_jwt: process.env.PINATA_JWT as string,
   pinata_gateway: process.env.GATEWAY_URL as string,
@@ -14,7 +14,7 @@ export async function GET(req: NextRequest, res: NextResponse) {
       post_url: `${process.env.BASE_URL}/frame`,
       buttons: [{ label: "Mint NFT", action: "post" }],
       aspect_ratio: "1:1",
-      cid: "QmNjEMqGKBcqfNRwx5xDoitXUHFfkM7N8nx6JLjHvbTMhR",
+      cid: "QmSYN7KT847Nado3fxFafYZgG6NXTMZwbaMvU9jhu5nPmJ",
     });
     return new NextResponse(frameMetadata);
   } catch (error) {
@@ -28,7 +28,6 @@ export async function POST(req: NextRequest, res: NextResponse) {
   const fid = body.untrustedData.fid;
   const address = await getConnectedAddressForUser(fid);
   const balance = await balanceOf(address);
-  const { isValid, message } = await fdk.validateFrameMessage(body);
   console.log(balance);
   if (typeof balance === "number" && balance !== null && balance < 1) {
     try {
@@ -36,17 +35,10 @@ export async function POST(req: NextRequest, res: NextResponse) {
       console.log(mint);
       const frameMetadata = await fdk.getFrameMetadata({
         post_url: `${process.env.BASE_URL}/redirect`,
-        buttons: [
-          { label: "Blog Tutorial", action: "post_redirect" },
-          { label: "Video Tutorial", action: "post_redirect" },
-        ],
+        buttons: [{ label: "Learn How to Make This", action: "post_redirect" }],
         aspect_ratio: "1:1",
-        cid: "QmNjEMqGKBcqfNRwx5xDoitXUHFfkM7N8nx6JLjHvbTMhR",
+        cid: "QmUx3kQH4vR2t7mTmW3jHJgJgJGxjoBsMxt6z1fkZEHyHJ",
       });
-      if (isValid) {
-        await fdk.sendAnalytics("frame-mint-tutorial-mint", body);
-      }
-
       return new NextResponse(frameMetadata);
     } catch (error) {
       console.log(error);
@@ -55,17 +47,10 @@ export async function POST(req: NextRequest, res: NextResponse) {
   } else {
     const frameMetadata = await fdk.getFrameMetadata({
       post_url: `${process.env.BASE_URL}/redirect`,
-      buttons: [
-        { label: "Blog Tutorial", action: "post_redirect" },
-        { label: "Video Tutorial", action: "post_redirect" },
-      ],
+      buttons: [{ label: "Learn How to Make This", action: "post_redirect" }],
       aspect_ratio: "1:1",
-      cid: "QmNjEMqGKBcqfNRwx5xDoitXUHFfkM7N8nx6JLjHvbTMhR",
+      cid: "QmaaEbtsetwamJwfFPAQAFC6FAE1xeYsvF7EBKA8NYMjP2",
     });
-    if (isValid) {
-      await fdk.sendAnalytics("frame-mint-tutorial-mint", body);
-    }
-
     return new NextResponse(frameMetadata);
   }
 }
